be8968fcbcb1b88a1eb2b13be86eedd777876067
rafaeljcamara <rafaelcamarac@gmail.com>
2022-01-20 10:50:46 +0000

    Add reverse string algorithms.

--- DSA/src/stringManipulationAlgorithms/stringManipulationAlgorithms.java
+++ DSA/src/stringManipulationAlgorithms/stringManipulationAlgorithms.java
@@ -2,10 +2,15 @@ package stringManipulationAlgorithms;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.Stack;
 
 public class stringManipulationAlgorithms {
 	
 	public static int countVowels(String string) {
+		/*
+		 * Time complexity: O(n)
+		 * Space complexity: O(1)
+		 * */
 		if(string==null) return 0;
 		int num = 0;
 		string = string.toLowerCase();
@@ -17,15 +22,69 @@ public class stringManipulationAlgorithms {
 		return num;
 	}
 	
+	public static String reverseString1(String string) {
+		/*
+		 * Time complexity: O(n)
+		 * Space complexity: O(n)
+		 * */
+		
+		if(string==null) throw new IllegalArgumentException("Please enter a valid string.");
+		String finalString = "";
+		Stack<Character> invertedString = new Stack<Character>();
+		
+		for(char c: string.toCharArray()) {
+			invertedString.push(c);
+		}
+		
+		while(!invertedString.isEmpty()) {
+			finalString+=Character.toString(invertedString.pop());
+		}
+		
+		return finalString;
+	}
+	
+	public static String reverseString2(String string) {
+		/*
+		 * Time complexity: O(n^2)
+		 * Space complexity: O(1)
+		 * */
+		 
+		if(string==null) throw new IllegalArgumentException("Please enter a valid string.");
+		String finalString = "";
+		for(int i=string.length()-1;i>=0;i--) {
+			finalString+=string.charAt(i);
+		}
+		return finalString;
+	}
+	
+	public static String reverseString3(String string) {
+		/*
+		 * Time complexity: O(n)
+		 * Space complexity: O(1)
+		 * */
+		 
+		if(string==null) throw new IllegalArgumentException("Please enter a valid string.");
+		StringBuilder finalString = new StringBuilder();
+		for(int i=string.length()-1;i>=0;i--) {
+			finalString.append(string.charAt(i));
+		}
+		return finalString.toString();
+	}
+	
 	private static boolean isVowel(char character) {
 		return character=='a' || character=='e' || character=='i' || character=='o' || character=='u';
 	}
 	
 	public static void main(String[] args) {
 		//testing countVowels
-		System.out.println(stringManipulationAlgorithms.countVowels("HEllo"));
-		System.out.println(stringManipulationAlgorithms.countVowels("jqty"));
-		System.out.println(stringManipulationAlgorithms.countVowels("Aeiou"));
+//		System.out.println(stringManipulationAlgorithms.countVowels("HEllo"));
+//		System.out.println(stringManipulationAlgorithms.countVowels("jqty"));
+//		System.out.println(stringManipulationAlgorithms.countVowels("Aeiou"));
+		
+		//testing reverseString
+		System.out.println(stringManipulationAlgorithms.reverseString1("Hello"));
+		System.out.println(stringManipulationAlgorithms.reverseString2("Hello"));
+		System.out.println(stringManipulationAlgorithms.reverseString3("Hello"));
 	}
 
 }
